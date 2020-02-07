e9aa8f4f3acf0e97b1ac93be31ee91f0e4d2e921
John Lockwood <JohnLockwood2999@gmail.com>
2020-02-06 20:05:49 -0500

    Latest zipfiles.py -- better construction of library root paths

--- layer/zipfiles.py
+++ layer/zipfiles.py
@@ -3,19 +3,18 @@ from poetry.factory import Factory
 from poetry.utils._compat import Path
 from poetry.utils.env import EnvManager
 
-
 poetry = Factory().create_poetry(Path.cwd())
 
-def get_poetry_venv_path():    
-    manager = EnvManager(poetry)
-    venv = manager.list()[0]
-    return venv.path
+def make_lib_path(root_dir, lib_dir):
+    '''Given a venv dir path at root_dir, and a library name at lib_dir, return full path'''
+    LIB_DIR_ROOT = "Lib"
+    LIB_DIR_PACKAGES  = 'site-packages'
+    return root_dir.joinpath(LIB_DIR_ROOT, LIB_DIR_PACKAGES, lib_dir)
 
-# def get_venv_path():
-#     stream = os.popen('poetry env list --full-path')    
-#     path_with_activated = stream.read()
-#     return path_with_activated
-
+def get_poetry_venv_paths():    
+    '''Get List of virutalEnv paths'''
+    manager = EnvManager(poetry)        
+    return [venv.path for venv in manager.list()]
 
 def get_packages():
     '''Return the non-dev poetry packages as a list of names'''
@@ -24,12 +23,23 @@ def get_packages():
     locked_repo = poetry.locker.locked_repository(include_dev)
     packages = locked_repo.packages
     return [package.name for package in packages ]
-    #pkgs.append("Hey Moe")
-    #pkgs.append("Hey Larry!")
-    #return pkgs
+
+def get_dev_library_directories():
+    '''Based on virtualEv'''
+    venvs = get_poetry_venv_paths()
+    # Assume libs are in first venv.
+    venv = venvs[0]
+    packages = get_packages()
+    dirs = []
+    for package in packages:
+        dirs.append(make_lib_path(venv, package))
+    return dirs
     
 
 if __name__ == "__main__":
-#    print("Shelling: ", get_venv_path())
-    print("Poetry: ", get_poetry_venv_path())
+    '''For now just prove the concept and exercise the functions'''
+    print("Poetry: ", get_poetry_venv_paths())
+    print("Type of path:  ", type(get_poetry_venv_paths()[0]))
     print(get_packages())
+    for dir in get_dev_library_directories():
+        print(dir)
